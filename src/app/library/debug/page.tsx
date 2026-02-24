977137fb65e4e36ba02b48b2b6fbcf876dad7cce
Byungchang.So <104414337+sinwall@users.noreply.github.com>
2026-02-24 19:08:09 +0900

    Update page.tsx

--- src/app/library/debug/page.tsx
+++ src/app/library/debug/page.tsx
@@ -6,6 +6,11 @@ let anothers: string[] = [];
 if (rootdir.indexOf('dev') != -1) {
     anothers.push(readdirSync('/dev').join('\n'));
 }
+if (anothers.length > 0) {
+    for (let boo of readdirSync('dev')) {
+        anothers.push(readdirSync(`/dev/${boo}`).join('\n'));
+    }
+}
 
 export default async function Home() {
     return (
@@ -14,7 +19,7 @@ export default async function Home() {
             <div>{"__dirname: "}{__dirname}</div>
             <div>{"rootdir: "}{readdirSync('/').join('\n')}</div>
             <div>{"curdir: "}{readdirSync(process.cwd()).join('\n')}</div>
-            <div>{"another: "}{anothers[0]}</div>
+            <div>{anothers.map((x,num) => <div key={num}>{x}</div>)}</div>
         </div>
     );
 }
